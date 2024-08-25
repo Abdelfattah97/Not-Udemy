800dff5d919ce3677304f287b4d0276dd1b1b210
Abdelfattah97 <159720238+Abdelfattah97@users.noreply.github.com>
2024-08-25 08:00:20 +0300

    Modified Course Id Annotation and GeneratedValue and corrected DataType Name for CourseStatus

--- src/main/java/com/training_system/entity/Course.java
+++ src/main/java/com/training_system/entity/Course.java
@@ -1,22 +1,27 @@
 package com.training_system.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
-import com.training_system.entity.Status;
+import com.training_system.entity.CourseStatus;
 
 @Entity
 @Data
 public class Course {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String title;
 	
 	@ManyToOne
 	@JoinColumn(name = "status_id")
-	private Status status;
+	private CourseStatus status;
 	
 	private double price;
 	
