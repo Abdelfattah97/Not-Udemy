527fdc78cb702052cd35dc77c6bb3e4f550af6e2
Abdelfattah97 <159720238+Abdelfattah97@users.noreply.github.com>
2024-09-10 10:39:42 +0300

    Allowed Spring Circular Refrences

--- src/main/java/com/training_system/controller/WalletController.java
+++ src/main/java/com/training_system/controller/WalletController.java
@@ -3,14 +3,13 @@ package com.training_system.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.training_system.entity.Person;
 import com.training_system.service.WalletService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
